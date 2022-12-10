caa1169289c0fef01c54a3564c1108a73cb85887
nbsdeveloper-lewis <75124231+nbsdeveloper-lewis@users.noreply.github.com>
2022-12-10 20:31:32 +0200

    Started implementing MoneyService

--- services/integrations/money_service.go
+++ services/integrations/money_service.go
@@ -13,3 +13,35 @@ type MoneyService interface {
 	TransferFrom(ctx context.Context, fromAccount dtos.AccountDto, amount decimal.Decimal) (transId *string, error error)
 	ReverseTransfer(ctx context.Context, transactionId string, amount decimal.Decimal) (error error)
 }
+
+type moneyService struct {
+}
+
+func (m moneyService) VerifyUser(ctx context.Context, userIdentifier, secretCode string) (user *string, error error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m moneyService) TransferTo(ctx context.Context, toAccount dtos.AccountDto, amount decimal.Decimal) (transId *string, error error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m moneyService) ReceiveFrom(ctx context.Context, toWalletId string, amount decimal.Decimal) (transId *string, error error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m moneyService) TransferFrom(ctx context.Context, fromAccount dtos.AccountDto, amount decimal.Decimal) (transId *string, error error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m moneyService) ReverseTransfer(ctx context.Context, transactionId string, amount decimal.Decimal) (error error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewMoneyService() MoneyService {
+	return &moneyService{}
+}
